aecca9b03ed874ac83ad9c69bb1552d5add5fbd0
HoangKhang5207 <hoangkhang16112003@gmail.com>
2024-09-28 15:08:59 +0700

    jsx - fragment - css in jsx

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,19 @@
 // () => {  }
 // Component = HTML + CSS + JavaScript
+//JSX
+//Fragment: 1 parent
+// Using CSS: className="" in JSX instead of class in HTML
+import './style.css';
+
 const MyComponent = () => {
     return (
-      <div>Hello Component & Khang Update</div>
+      <>
+        <div>Hello Component & Khang Update</div>
+        <div className="child"
+                style={
+                    {borderRadius: "10px"}
+                }>child</div>
+      </>
     );
 }
 
