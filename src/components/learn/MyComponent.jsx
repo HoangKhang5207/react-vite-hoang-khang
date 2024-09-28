b2ac944697ddfc094e39b517665fe0daa516c354
HoangKhang5207 <hoangkhang16112003@gmail.com>
2024-09-28 17:01:47 +0700

    use variable in JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,12 +3,28 @@
 //JSX
 //Fragment: 1 parent
 // Using CSS: className="" in JSX instead of class in HTML
+// Using Variable in JSX: {variable-name}
+
 import './style.css';
 
 const MyComponent = () => {
+    // const khang = "khangne"; string
+    // const khang = 22; number
+    // const khang = true; boolean -> no display on HTML
+    // const khang = undefined; undefined -> no display on HTML
+    // const khang = null; null -> no display on HTML
+
+    // const khang = [1, 2, 3]; // -> no display correctly with array on HTML
+    const khang = {
+        name: "khangne",
+        age: 22
+    }; // -> error: no display the object on HTML but {khang.name} or {khang.age} is valid
+    // using JSON.stringify to display the object on HTML as a JSON string
+
     return (
       <>
-        <div>Hello Component & Khang Update</div>
+        <div> {JSON.stringify(khang)} Hello Component & Khang Update</div>
+        <div>{console.log("KHANG")}</div>
         <div className="child"
                 style={
                     {borderRadius: "10px"}
